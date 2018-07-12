d2d2de1bc4b61f969391602c20d5d8f5bd7c7ae4
spartchou <spart.chou@gmail.com>
2018-07-13 01:39:19 +0800

    Generate a User class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_12_172456) do
+ActiveRecord::Schema.define(version: 2018_07_12_173415) do
 
   create_table "blorgh_articles", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2018_07_12_172456) do
   create_table "blorgh_comments", force: :cascade do |t|
     t.integer "article_id"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
